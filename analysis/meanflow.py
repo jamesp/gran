1ffbfbc9e58800fcafe0099f0bd81f4000fa7cba
James Penn <james@jamespenn.co.uk>
2016-03-04 12:47:28 +0000

    moved meanflow.py to analysis

--- analysis/meanflow.py
+++ analysis/meanflow.py
@@ -7,8 +7,8 @@ def eddy_kinetic_energy(dataset):
     """Calculate the total Eddy Kinetic Energy, per unit area of surface"""
     pcoord = 'pfull' if 'pfull' in dataset.ucomp.coords.keys() else 'phalf'
     ubar, uprime = mean_and_eddy(dataset.ucomp)
-    vbar, vprime = mean_and_eddy(dataset.ucomp)
+    vbar, vprime = mean_and_eddy(dataset.vcomp)
     eke = 0.5*(uprime**2 + vprime**2).sum(pcoord)
     eke.name = 'eke'
-    eke.long_name = 'eddy kinetic energy'
+    #eke.long_name = 'eddy kinetic energy'
     return eke
